47d448e90265a11e47c951bed6f91a6dee69714b
Giovana <giomalaqui@gmail.com>
2022-03-15 01:41:18 -0300

    Exercicio 13A concluido

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -102,7 +102,7 @@ function retornaPessoaAnonimizada(pessoa) {
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+    return pessoas.filter((pessoa) => pessoa.idade > 14 && pessoa.altura >= 1.5 && pessoa.idade < 60)
 }
 
 // EXERCÍCIO 13B
