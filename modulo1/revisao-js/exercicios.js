9f67932d3867e923ff1eef6c42ffb191343cd39a
Giovana <giomalaqui@gmail.com>
2022-03-15 00:04:11 -0300

    Exercicio 7 concluido

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -44,7 +44,21 @@ function retornaMaiorNumero(array) {
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
-
+   let maiorNumero
+   let menorNumero
+   if (num1 > num2){
+       maiorNumero = num1
+       menorNumero = num2
+   } else {
+       maiorNumero = num2
+       menorNumero = num1
+   }
+   let diferenca = maiorNumero - menorNumero
+   return {
+       maiorNumero: maiorNumero,
+       maiorDivisivelPorMenor: maiorNumero % menorNumero === 0,
+       diferenca: maiorNumero - menorNumero
+   }
 }
 
 // EXERCÍCIO 08
