9d5e9131e00385d779f1713fbecc797d5b209dcc
Giovana <giomalaqui@gmail.com>
2022-04-30 23:25:01 -0300

    Concluido projeto fixação Whatslab

--- modulo3/projeto-whatslab/whatslab/src/App.js
+++ modulo3/projeto-whatslab/whatslab/src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import styled from 'styled-components'
+import Chat from './components/Chat'
+ 
+const ContainerApp = styled.div`
+display:flex;
+flex-direction:column;
+height:95vh;
+width:40%;
+margin: 0 auto;
+background-image:url(https://todaatual.com/wp-content/uploads/584e2a560e84bdb9216bdee4f43d9e93.jpg);
+background-repeat:no-repeat;
+background-position:center bottom;
+background-size:cover;
+border:1px solid black;
+border-radius:20px;
+padding:10px;
+justify-content:flex-end;
+`;
 
 function App() {
   return (
+
     <div className="App">
-      
+      <ContainerApp>
+        <Chat/>
+      </ContainerApp>
     </div>
   );
 }
