da4bb5d5f9597983beebfd27629474fa4494eb66
Giovana Santos <giomalaqui@gmail.com>
2022-10-18 21:44:23 -0300

    Arquitetura de software 3

---
+++ modulo6/arquitetura-de-software-3/src/business/MovieBusiness.ts
@@ -0,0 +1,26 @@
+import { MovieDatabase } from "../data/MovieDatabase"
+import { v4 as generateId } from 'uuid'
+import { Movie } from "../types/Movie"
+
+export class MovieBusiness {
+  async create({ title, description, duration_in_minutes, year_of_release }: any):Promise<void> {
+    if (!title || !description || !duration_in_minutes || !year_of_release) {
+      throw new Error("Dados inválidos (title, description, duration_in_minutes, year_of_release)")
+    }
+
+    const id = generateId()
+
+    const movieDatabase = new MovieDatabase()
+    await movieDatabase.create({
+      id,
+      title,
+      description,
+      duration_in_minutes,
+      year_of_release
+    })
+  }
+  async get():Promise<Movie[]> {
+    const movieDatabase = new MovieDatabase()
+    return await movieDatabase.get()
+  }
+}
---
+++ modulo6/arquitetura-de-software-3/src/data/UserDatabase.ts
@@ -0,0 +1,25 @@
+import { User } from "../types/User";
+import { BaseDatabase } from "./BaseDatabase";
+
+export class UserDatabase extends BaseDatabase {
+  private static TABLE_NAME = "LABEFLIX_USER";
+
+  async get():Promise<User[]>{
+    const result = await UserDatabase
+    .connection(UserDatabase.TABLE_NAME)
+    .select()
+
+    return result
+  }
+
+  async create({ id, name, email, password }: any): Promise<void> {
+    await UserDatabase.connection
+      .insert({
+        id,
+        name,
+        email,
+        password,
+      })
+      .into(UserDatabase.TABLE_NAME);
+  }
+}
